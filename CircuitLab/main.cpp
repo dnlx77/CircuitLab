60f5c5292e7b7888a6fa9f9e61a3d342af7afcf0
dnlx77 <dnlx77@gmail.com>
2026-03-26 19:35:22 +0100

    feat: add working main.cpp with SFML + ImGui integration

--- CircuitLab/main.cpp
+++ CircuitLab/main.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <imgui-SFML.h>
+#include <imgui.h>
 
 int main()
 {
@@ -6,18 +8,35 @@ int main()
         sf::VideoMode({ 800u, 600u }),
         "CircuitLab"
     );
+    window.setFramerateLimit(60);
+
+    if (!ImGui::SFML::Init(window))
+        return -1;
+
+    sf::Clock deltaClock;
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+            ImGui::SFML::ProcessEvent(window, *event);
+
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        window.clear(sf::Color::Black);
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        // Finestra di test ImGui
+        ImGui::Begin("CircuitLab - Test");
+        ImGui::Text("SFML + ImGui funzionano!");
+        ImGui::End();
+
+        window.clear(sf::Color(30, 30, 30));
+        ImGui::SFML::Render(window);
         window.display();
     }
 
+    ImGui::SFML::Shutdown();
     return 0;
 }
